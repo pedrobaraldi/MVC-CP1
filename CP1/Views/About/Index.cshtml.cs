1ef886d4fba240e24846d236d5580a896c724aca
pedrobaraldi <pedrobaraldi02@gmail.com>
2024-03-20 17:55:17 -0300

    Adicionar arquivos de projeto.

---
+++ CP1/Views/About/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CP1.Views.About
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ CP1/Views/Contact/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CP1.Views.Contact
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
